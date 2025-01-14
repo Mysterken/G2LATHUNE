b0d0d48e677058d0515b86dbe6e3633131301ff1
Mysterken <Mysterkenzo@gmail.com>
2025-01-14 10:41:28 +0100

    add template authRoutes

--- backend/src/routes/v1/authRoutes.js
+++ backend/src/routes/v1/authRoutes.js
@@ -6,8 +6,28 @@ router.get('/login', (req, res) => {
     res.send('Login route');
 });
 
-router.get('/register', (req, res) => {
+router.get('/logout', (req, res) => {
+    res.send('Logout route');
+});
+
+router.post('/register', (req, res) => {
     res.send('Register route');
+});
+
+router.post('/refresh', (req, res) => {
+    res.send('Refresh route');
+});
+
+router.post('/forgot-password', (req, res) => {
+    res.send('Forgot password route');
+});
+
+router.post('/reset-password', (req, res) => {
+    res.send('Reset password route');
+});
+
+router.get('/verify-email', (req, res) => {
+    res.send('Verify email route');
 });
 
 module.exports = router;
