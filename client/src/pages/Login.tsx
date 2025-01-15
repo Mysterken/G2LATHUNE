08d02dc6710da43d07d7582087381dba6b844ffb
ZAKHARY Mina <85484181+MinaZA@users.noreply.github.com>
2025-01-15 12:19:41 +0100

    popup_error_message_login_front

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import API from "../services/api";
 import { useAuth } from "../hooks/useAuth";
 import { Navigate } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
 
 const Login = () => {
     const { user, login } = useAuth();
@@ -17,6 +18,16 @@ const Login = () => {
             localStorage.setItem("token", data.accessToken);
             console.log(data);
         } catch (error) {
+            toast.error("Erreur lors de la connection", {
+                position: "bottom-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: false,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "dark",
+                });
             console.error("Login failed");
         }
     };
@@ -45,6 +56,19 @@ const Login = () => {
             <button type="submit" className="w-full bg-blue-500 text-white p-2 rounded">
                 Login
             </button>
+            <ToastContainer
+                position="bottom-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick={false}
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="dark"
+                />
+
         </form>
     );
 };
