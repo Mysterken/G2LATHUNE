679dc59e1029650611400bcc67396cbf468dab64
Nicolas Speich <speich@MacBook-Pro-de-Nicolas-2.local>
2025-01-17 14:42:30 +0100

    code mort en moins

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,32 +1,3 @@
-// const app = require("./server");
-// const { port } = require("./config");
-
-// const server = app.listen(port, function () {
-//   console.log(`Webserver is ready and running on port ${port}`);
-// });
-
-// // Graceful shutdown on Docker signals
-// process.on("SIGINT", function onSigint() {
-//   console.info("Got SIGINT (ctrl-c). Graceful shutdown ", new Date().toISOString());
-//   shutdown();
-// });
-
-// process.on("SIGTERM", function onSigterm() {
-//   console.info("Got SIGTERM (docker stop). Graceful shutdown ", new Date().toISOString());
-//   shutdown();
-// });
-
-// function shutdown() {
-//   server.close(function onServerClosed(err) {
-//     if (err) {
-//       console.error(err);
-//       process.exit(1);
-//     }
-//     process.exit(0);
-//   });
-// }
-
-
 const app = require("./server");
 const { port } = require("./config");
 
--- backend/src/routes/v1/walletRoutes.js
+++ backend/src/routes/v1/walletRoutes.js
@@ -1,27 +1,3 @@
-// const express = require("express");
-// const router = express.Router();
-// const database = require("../../database");
-
-// // Transaction data
-// router.get("/get-data", async (req, res) => {
-//   try {
-//     const transactions = await database("transactions").select("date", "price");
-
-//     res.json({
-//       success: true,
-//       data: transactions,
-//     });
-//   } catch (error) {
-//     console.error(`Erreur lors de la récupération des données : ${error.message}`);
-//     res.status(500).json({ success: false, error: "Erreur interne du serveur" });
-//   }
-// });
-
-// module.exports = router;
-
-
-
-
 const express = require("express");
 const router = express.Router();
 const database = require("../../database");
