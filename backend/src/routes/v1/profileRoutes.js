8e6ed71d96c2e4ec51c1d51de0ba0bfa3ea3f6fc
Mysterken <Mysterkenzo@gmail.com>
2025-01-17 03:16:31 +0100

    update wallet route

--- backend/src/routes/v1/profileRoutes.js
+++ backend/src/routes/v1/profileRoutes.js
@@ -28,4 +28,30 @@ router.get('/get_wallet', async (req, res) => {
     res.json({ wallet });
 });
 
+router.put('/update_wallet', async (req, res) => {
+    if (!req.headers.authorization) {
+        return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    const token = req.headers.authorization.split(' ')[1];
+    const decoded = jwt.verify(token, 'secret');
+    const email = decoded.email;
+
+    const sql = "SELECT * FROM User WHERE email = ?";
+    let [user] = await database.raw(sql, [email])
+
+    if (!user || user.length === 0) {
+        return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    user = user[0];
+
+    const wallet = req.body.wallet || "";
+
+    const updateSql = "UPDATE User SET wallet = ? WHERE email = ?";
+    await database.raw(updateSql, [wallet, email]);
+
+    res.json({ wallet });
+});
+
 module.exports = router;
